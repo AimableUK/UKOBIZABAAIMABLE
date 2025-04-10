2aaf8da8653f45ee66a3cbd2753b5b56d1629047
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-10 11:07:22 +0200

    added another project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import malosFlix from '../../assets/malosFlix.png';
+import malosCore from '../../assets/malosCore.png';
 
 const Projects = () => {
   return (
@@ -10,6 +11,16 @@ const Projects = () => {
           <a href='https://malos-flix.vercel.app/' target='_blank'>
             <img src={malosFlix} alt='malosFLix Project' className="w-full h-auto object-cover border-2 rounded-md border-black" />
             <p className='font-bold'>MalosFlix - Movie Database</p>
+            <p className='font-semibold'>Visit Website</p>
+          </a>
+          
+        </div>
+        <div
+          className="cursor-pointer w-full flex flex-col items-start border-2 border-primaryBorder p-3 rounded-md shadow-lg transition-transform duration-300 hover:translate-y-2"
+        >
+          <a href='https://malos-core.vercel.app/' target='_blank'>
+            <img src={malosCore} alt='malosFLix Project' className="w-full h-auto object-cover border-2 rounded-md border-black" />
+            <p className='font-bold'>Malos Core - Admin Panel</p>
             <p className='font-semibold'>Visit Website</p>
           </a>
           
