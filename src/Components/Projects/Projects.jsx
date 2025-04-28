bfe7a8df8411c0af91f4342f2c24b080feadbcce
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-28 13:50:23 +0200

    added another site in development

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import malosFlix from '../../assets/malosFlix.png';
 import malosCore from '../../assets/malosCore.png';
+import malosHaven from '../../assets/malosHaven.png';
 
 const Projects = () => {
   return (
@@ -22,6 +23,16 @@ const Projects = () => {
             <img src={malosCore} alt='malosFLix Project' className="w-full h-auto object-cover border-2 rounded-md border-black" />
             <p className='font-bold'>Malos Core - Admin Panel</p>
             <p className='font-semibold'>Visit Website</p>
+          </a>
+          
+        </div>
+        <div
+          className="cursor-pointer w-full flex flex-col items-start border-2 border-primaryBorder p-3 rounded-md shadow-lg transition-transform duration-300 hover:translate-y-2"
+        >
+          <a href='#'>
+            <img src={malosHaven} alt='malosFLix Project' className="w-full h-auto object-cover border-2 rounded-md border-black" />
+            <p className='font-bold'>Malos Haven - Tenant Management</p>
+            <p className='font-semibold'>Still in devpt</p>
           </a>
           
         </div>
