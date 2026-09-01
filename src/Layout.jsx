5e4f7048e1fb152483ea43f4f91f3759f2ccae1f
AimableUK <ukobizaba81@gmail.com>
2026-09-01 13:53:43 +0200

    Refactor code style in Menu and Layout components

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useRef} from 'react'; 
-import { Outlet, useLocation} from 'react-router-dom';
-import Details from './Components/Details/Details';
-import Menu from './Components/Menu/Menu';
-import DarkMode from './Components/DarkMode/DarkMode'
-import PageTitleUpdater from './Components/TitleUpdater/TitleUpdater';
-import { Analytics } from "@vercel/analytics/react"
+import React, { useEffect, useRef } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Details from "./Components/Details/Details";
+import Menu from "./Components/Menu/Menu";
+import DarkMode from "./Components/DarkMode/DarkMode";
+import PageTitleUpdater from "./Components/TitleUpdater/TitleUpdater";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = () => {
   const location = useLocation();
@@ -25,12 +25,14 @@ const Layout = () => {
       {/* Small Devices (Mobile) */}
       <div className="flex flex-col gap-10 justify-between items-center md:items-start mt-10 mb-20 md:m-20 ml-5 lg:ml-12 mr-5  lg:mr-12 md:hidden">
         <Details />
-        <section 
+        <section
           ref={outletRef}
-          className={`mystyle w-fit flex flex-col items-center md:w-2/4 mx-auto p-2 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white 
-          ${isProjects ? 'overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide' : ''}`}
-        > 
-          <div className='mb-1 self-end'><DarkMode /></div>
+          className={`mystyle w-fit flex flex-col items-center md:w-2/4 mx-auto p-2 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white
+          ${isProjects ? "overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide" : ""}`}
+        >
+          <div className="mb-1 self-end">
+            <DarkMode />
+          </div>
           <Outlet />
         </section>
         <Menu /> {/* Menu stays at the bottom */}
@@ -38,13 +40,15 @@ const Layout = () => {
 
       {/* Medium Devices (Tablets) */}
       <div className="hidden md:flex flex-col gap-10 justify-between m-20 ml-12 mr-12 lg:hidden">
-        <div className="flex flex-row gap-10 justify-between">  
+        <div className="flex flex-row gap-10 justify-between">
           <Details />
-          <section 
-            className={`mystyle min-w-48 flex flex-col items-center w-2/4 mx-auto p-6 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white 
-            ${isProjects ? 'overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide' : ''}`}
-          > 
-          <div className='mb-1 self-end'><DarkMode /></div>
+          <section
+            className={`mystyle min-w-48 flex flex-col items-center w-2/4 mx-auto p-6 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white
+            ${isProjects ? "overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide" : ""}`}
+          >
+            <div className="mb-1 self-end">
+              <DarkMode />
+            </div>
             <Outlet />
           </section>
         </div>
@@ -54,19 +58,18 @@ const Layout = () => {
       {/* Large Devices (Desktops) */}
       <div className="hidden lg:flex flex-row gap-10 justify-between m-20 ml-12 mr-12">
         <Details />
-        <section 
-        
-          className={`mystyle min-w-48 flex flex-col items-center w-2/4 mx-auto p-6 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white 
-          ${isProjects ? 'overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide' : ''}`}
-        > 
-        <div className='mb-1 self-end'><DarkMode /></div>
+        <section
+          className={`mystyle min-w-48 flex flex-col items-center w-2/4 mx-auto p-6 text-gray-800 border-primaryBorder bg-primaryCard border-2 rounded-md bg-white
+          ${isProjects ? "overflow-hidden overflow-y-auto max-h-[75vh] scrollbar-hide" : ""}`}
+        >
+          <div className="mb-1 self-end">
+            <DarkMode />
+          </div>
           <Outlet />
         </section>
         <Menu />
       </div>
-    </div> 
-
-
+    </div>
   );
 };
 
