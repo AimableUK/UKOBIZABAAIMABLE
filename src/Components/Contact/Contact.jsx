bad2ec2f6efefb3b448a728a1e4742a9884ad326
AimableUK <ukobizaba81@gmail.com>
2026-09-02 12:53:37 +0200

    Remove unnecessary React import from Contact component

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Formik, Field, ErrorMessage, Form } from "formik";
 import SubmitSchema from "../ContactSchema/SubmitSchema";
 import emailjs from "@emailjs/browser";
